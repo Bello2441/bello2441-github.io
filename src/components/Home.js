11624600f12b496bd2eaffdfba6df041fad1b64a
Bello2441 <rosenbergbello.@lc.cuny.edu'j>
2020-11-25 01:12:31 -0500

    still trying to add ainmation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import logo from './the_hulk.jpeg';
-import { bounce } from 'react-animations';
-import Radium, {StyleRoot} from 'radium';
 
-const styles = {
-    bounce : {
-        animation : 'x 1s',
-        animationName: Radium.keyframes(bounce, 'bounce')
-    }
-}
 class Home extends React.Component {
     render() {
         return (
-            <div className="App">
+        <div className="App">
             <h1 className="headStyle">Home page</h1>
             <p>Hello, my name is Rosenberg Bello and this is my portfolio.</p>
             <p>This is my first multi-page website using react!</p>
             <p>I hope anyone viewing enjoys what I did</p> 
             <p>and I will update as soon as I can.</p>    
-                <img className="homeImg" src={logo} alt="image_of_me" />
-                <StyleRoot>
-                    <div style={styles.bounce}>
-                    </div>
-                </StyleRoot>
+            <img className="homeImg" src={logo} alt="image_of_me" />
+            <h1 className="nameplate" class="animate__animated animate__bounce">What's up viewer!!</h1>
         </div>
         
         );
