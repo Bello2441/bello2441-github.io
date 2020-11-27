be24c7118700e796a65f0ec197d2c64219412ec2
Rosenberg Bello <rosenbergbello@Rosenbergs-MacBook-Pro.local>
2020-11-26 20:11:23 -0500

    have an animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './the_hulk.jpeg';
+import { Spring } from 'react-spring/renderprops';
 
 class Home extends React.Component {
     render() {
@@ -11,11 +12,28 @@ class Home extends React.Component {
             <p>I hope anyone viewing enjoys what I did</p> 
             <p>and I will update as soon as I can.</p>    
             <img className="homeImg" src={logo} alt="image_of_me" />
-            <h1 className="nameplate" class="animate__animated animate__bounce">What's up viewer!!</h1>
+            <Spring
+            from={{ opacity: 0, marginTop: -500}}
+            to={{ opacity: 1, marginTop : 0}}
+        >
+            {props => (
+                <div style={props}>
+                    <div style={c1Style}>
+                        <h1>Hello Viewer! How you doing today?</h1>
+                    </div>
+                </div>
+            )}
+        </Spring>
         </div>
         
         );
     }
+}
+
+const c1Style ={
+    background : 'red',
+    color : 'yellow',
+    padding : '1.5rem'
 }
 
 export default Home;
