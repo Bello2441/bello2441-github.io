85ff2d01acad959b940f88d5907e14252448d6a5
Rosenberg Bello <rosenbergbello@Rosenbergs-MacBook-Pro.local>
2020-11-26 21:30:01 -0500

    added animatoion to about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import file from './Indeed_Resume.pdf';
+import { Spring } from 'react-spring/renderprops';
 
 function About() {
     return (
-        <div className="App">
+        <div className="aboutStyle">
                 <h1 className="About">About Me</h1>
             <p>
                 My name is Rosenberg Bello and I am undergraduate at Lehman College at the moment.
@@ -24,16 +25,24 @@ function About() {
                 But my dream is to be a jack of all trades and be good at every subject the world may throw at me. I feel like working on multiple and extremely different jobs would fun as hell.
             </p>
             <p>
-                My hobbies are to watch baseball, play some videogames and study Japanese when I have the time. Right now, I am watching a Russian TV mini series called "The Meeting Place can not be Changed".
-            </p>
-            <p>
-                If you reader are bored and want something to watch, then I would give this a look. It's about bad cop who believes a criminal should just be in jail and a good cop who believes that people can change. 
-            <a href="https://www.youtube.com/watch?v=X7Kr8g8wps0" alt="Russian_TV_series"> The Meeting Place cannot be Changed episode 1</a>
+                My hobbies are to watch baseball, play some videogames and study Japanese when I have the time. 
             </p>
             <p>
                 Anyways, I will give a link to download my resume so you guys can look at it and either give me some info on how to make it better or if you want a template.
             </p>
             <a href={file} alt="Rosenberg's_Resume">Take a look at my resume!</a>
+            <Spring
+            from={{ opacity: 0, marginTop: -500}}
+            to={{ opacity: 1, marginTop : 0}}
+        >
+            {props => (
+                <div style={props}>
+                    <div className="About">
+                        <h3>If you want more information about me, go to the Contact me page</h3>
+                    </div>
+                </div>
+            )}
+        </Spring>
         </div>
     );
 }
