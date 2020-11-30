277fb37870a33fbbaddfd9ed3b8639a0dd9de627
Bello2441 <rosenbergbello.@lc.cuny.edu'j>
2020-11-30 00:01:17 -0500

    few finishing touches

--- src/components/subpages/Booklist-project.js
+++ src/components/subpages/Booklist-project.js
@@ -14,7 +14,7 @@ function Project_3() {
                         <img className="projectImg" src={logo} alt="practice_project"/>
                         <p>This is a project that I have on the side and mainly used for practice.</p>
                         <p>I am following a ten-hour crash course video on react and this is what the project is about.</p>
-                        <p>While making this, I thought "why not add it to my github?" So when I am done with it, people can bsee how good it looks.</p>
+                        <p>While making this, I thought "why not add it to my github?" So when I am done with it, people can see how good it looks.</p>
                         <a href="https://github.com/Bello2441/Booklist">Click here to see the repo on Github</a>
                     </div>
                 </div>
