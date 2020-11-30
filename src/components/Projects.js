6bd0b3b75959dde3f5df2aff0b8f66ee6ddbf84e
Bello2441 <rosenbergbello.@lc.cuny.edu'j>
2020-11-29 23:56:30 -0500

    added last project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,18 +2,22 @@ import React from 'react';
 import {Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
 import Project_1 from './subpages/CMP464-HW2';
 import Project_2 from './subpages/CMP464-HW3';
+import Project_3 from './subpages/Booklist-project';
 
 function Projects() {
     return (
         <Router>
         <nav className="projectIntro">
-        <h1 className="projectstyle">Projects I have done</h1>
+        <h1 style = {{color : "white"}}>Projects I have done</h1>
             <ul>
                 <li>
                     <Link to="/projects/project1">CMP464-HW2</Link>
                 </li>
                 <li>
                     <Link to="/projects/project2">CMP464-HW3</Link>
+                </li>
+                <li>
+                    <Link to="/projects/project3">Booklist Project</Link>
                 </li>
             </ul>
             <p style={{color : "whitesmoke"}}> 
@@ -23,6 +27,7 @@ function Projects() {
         <Switch>
             <Route path="/projects/project1" exact component={Project_1}/>
             <Route path="/projects/project2" component={Project_2}/>
+            <Route path="/projects/project3" component={Project_3}/>
         </Switch>
         </Router>
     );
