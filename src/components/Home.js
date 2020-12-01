6e99a7ec1cda2f2cb96dd16b6f72a209c445b704
Bello2441 <rosenbergbello.@lc.cuny.edu'j>
2020-12-01 00:03:28 -0500

    messed up

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,13 +13,26 @@ class Home extends React.Component {
             <p className="p-color">and I will update as soon as I can.</p>    
             <img className="homeImg" src={logo} alt="image_of_me" />
             <Spring
-            from={{ opacity: 0, marginTop: -500}}
-            to={{ opacity: 1, marginTop : 0}}
+            from={{ opacity: 0}}
+            to={{ opacity: 1}}
+            confidg={{delay : 2000, duration : 2000}}
         >
             {props => (
                 <div style={props}>
-                    <div style={c1Style}>
+                    <div style={c2Style}>
                         <h1>Hello Viewer! How you doing today?</h1>
+                        <p>Wait until the counter is up to get a surprise...</p>
+                        <Spring 
+                        from = {{number : 0}}
+                        to = {{number : 86400}}
+                        config={{duration : 86400000}}
+                        >
+                            {props => (
+                                <div style={props}>
+                                    <h1 style={counter}>{props.number.toFixed()}</h1>
+                                </div>
+                            )}
+                        </Spring>
                     </div>
                 </div>
             )}
@@ -30,10 +43,19 @@ class Home extends React.Component {
     }
 }
 
-const c1Style ={
-    background : 'red',
+const c2Style ={
+    background : 'rgb(209, 223, 159)',
     color : 'yellow',
     padding : '1.5rem'
 }
 
+const counter = {
+    background : '#333',
+    textAlign : 'center',
+    width : '100px',
+    borderRadius : '50%',
+    margin : '1rem auto'
+}
+
 export default Home;
+
