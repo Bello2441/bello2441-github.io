46d4c5f65f244fa63b0c2d1a83f2e2fa70627b65
Bello2441 <rosenbergbello.@lc.cuny.edu'j>
2020-11-30 23:42:09 -0500

    Updates

---
+++ precache-manifest.0f77df2a96d70b2619074d2b55b93a89.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ace480a635c4f3df9bfb684a1a81a53",
+    "url": "/bello2441-github.io/index.html"
+  },
+  {
+    "revision": "dad8f4596755a3478a8f",
+    "url": "/bello2441-github.io/static/css/main.eb4e02ee.chunk.css"
+  },
+  {
+    "revision": "d2111df238409b9ac35c",
+    "url": "/bello2441-github.io/static/js/2.a34df929.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bello2441-github.io/static/js/2.a34df929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad8f4596755a3478a8f",
+    "url": "/bello2441-github.io/static/js/main.500ee411.chunk.js"
+  },
+  {
+    "revision": "9049a562a5ee699bb293",
+    "url": "/bello2441-github.io/static/js/runtime-main.c328a4a4.js"
+  },
+  {
+    "revision": "9c6d600b136ac8587095ca110f3f6820",
+    "url": "/bello2441-github.io/static/media/Booklist.9c6d600b.png"
+  },
+  {
+    "revision": "08fcc532caabfd53162dc55886b28e5f",
+    "url": "/bello2441-github.io/static/media/HW2.08fcc532.png"
+  },
+  {
+    "revision": "9c7ad3834757c1a529b7cbb01d0c1069",
+    "url": "/bello2441-github.io/static/media/Indeed_Resume.9c7ad383.pdf"
+  },
+  {
+    "revision": "8a06fda377c8b5ab9fc8a208b318e7f9",
+    "url": "/bello2441-github.io/static/media/design-pad.8a06fda3.png"
+  },
+  {
+    "revision": "a3ae082704a6aee3ff50beef6b7b09fd",
+    "url": "/bello2441-github.io/static/media/the_hulk.a3ae0827.jpeg"
+  }
+]);
