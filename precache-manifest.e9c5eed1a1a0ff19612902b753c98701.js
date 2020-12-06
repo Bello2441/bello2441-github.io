8f76ca0fd792437aea7da7c2d29c93a3a45a97da
Bello2441 <rosenbergbello.@lc.cuny.edu'j>
2020-12-06 12:47:48 -0500

    Updates

---
+++ precache-manifest.e9c5eed1a1a0ff19612902b753c98701.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10a2d6774b764937f5d4114272aa9f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "988d0ec4d8181656b5fd",
+    "url": "/static/css/main.eb4e02ee.chunk.css"
+  },
+  {
+    "revision": "d2111df238409b9ac35c",
+    "url": "/static/js/2.a34df929.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a34df929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988d0ec4d8181656b5fd",
+    "url": "/static/js/main.c215b423.chunk.js"
+  },
+  {
+    "revision": "752c69007ba52d3371fd",
+    "url": "/static/js/runtime-main.2799a32a.js"
+  },
+  {
+    "revision": "9c6d600b136ac8587095ca110f3f6820",
+    "url": "/static/media/Booklist.9c6d600b.png"
+  },
+  {
+    "revision": "08fcc532caabfd53162dc55886b28e5f",
+    "url": "/static/media/HW2.08fcc532.png"
+  },
+  {
+    "revision": "9c7ad3834757c1a529b7cbb01d0c1069",
+    "url": "/static/media/Indeed_Resume.9c7ad383.pdf"
+  },
+  {
+    "revision": "8a06fda377c8b5ab9fc8a208b318e7f9",
+    "url": "/static/media/design-pad.8a06fda3.png"
+  },
+  {
+    "revision": "a3ae082704a6aee3ff50beef6b7b09fd",
+    "url": "/static/media/the_hulk.a3ae0827.jpeg"
+  }
+]);
